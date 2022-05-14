0800094eb4ddc9acb932b0110da077bb34472576
Chris Rowe <rowe2ry@yahoo.com>
2022-05-14 11:20:50 -0400

    Created the user model

---
+++ Models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: { type: String , required: true, unique: true, trim: true },
+        email: { type: String, required: true, unique: true, validate: {
+            validator: function(email) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            }, message: props => `${props} is not a valid email address.`
+        } },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        ]
+    },
+    {
+        toJson: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount')
+.get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
